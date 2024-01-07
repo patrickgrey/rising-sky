aafb50cff11d8584056e0b9e4145bd2739bbde48
patrickgrey <websemantic@yahoo.co.uk>
2024-01-07 16:14:24 +0000

    Layout and scales added

---
+++ website-build/js/main.js
@@ -0,0 +1,20 @@
+console.log("base main js loaded")
+
+// Get current hour (24)
+// Need morning, day and night colours for light and dark themes
+// Set custom properties depending.
+
+/*const date = new Date();
+const hour = date.getHours();
+
+if (hour < 12) {
+  body.style.backgroundColor = 'lightblue';
+} else if (hour < 18) {
+  body.style.backgroundColor = 'orange';
+} else {
+  body.style.backgroundColor = 'black';
+} */
+
+// Morning is orange to blue
+// Day is dark to light blue
+// 
