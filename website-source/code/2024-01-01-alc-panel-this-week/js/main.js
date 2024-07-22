74f3bfdde5ec024cdd5a46135f2e82b6e26c2171
patrickgrey <websemantic@yahoo.co.uk>
2024-07-22 16:50:13 +0100

    Image replace with video working

--- website-source/code/2024-01-01-alc-panel-this-week/js/main.js
+++ website-source/code/2024-01-01-alc-panel-this-week/js/main.js
@@ -1,4 +1,20 @@
 // https://adrianroselli.com/2024/06/youtube-and-vimeo-web-component.html
 // https://github.com/luwes/lite-vimeo-embed/blob/main/lite-vimeo-embed.js
 // https://scottjehl.com/posts/even-responsiver-video/
-console.log("ello");
+const rsHeroImage = document.querySelector(`img[data-id="rsHeroImage"]`).closest("picture")
+const button = document.createElement("button")
+
+button.classList.add("rs-img-hero")
+button.classList.add("rs-button-unset")
+
+button.style.maxWidth = rsHeroImage.style.maxWidth;
+
+rsHeroImage.parentElement.append(button)
+button.append(rsHeroImage)
+
+button.addEventListener("click", function (event) {
+    const vimeo = document.createElement("lite-vimeo")
+    vimeo.setAttribute("videoid", "988437576")
+    button.replaceWith(vimeo)
+    vimeo.click()
+})
