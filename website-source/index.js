37c3f7e1506fa0e8359db20df468605b8fbb5f4d
patrickgrey <websemantic@yahoo.co.uk>
2024-09-16 14:42:59 +0100

    Now rising up

--- website-source/index.js
+++ website-source/index.js
@@ -1,43 +1,6 @@
 (function () {
 
     /*
-    ##     ## #### ########  ########  #######
-    ##     ##  ##  ##     ## ##       ##     ##
-    ##     ##  ##  ##     ## ##       ##     ##
-    ##     ##  ##  ##     ## ######   ##     ##
-     ##   ##   ##  ##     ## ##       ##     ##
-      ## ##    ##  ##     ## ##       ##     ##
-       ###    #### ########  ########  #######
-    */
-
-    // https://adrianroselli.com/2024/06/youtube-and-vimeo-web-component.html
-    // https://github.com/luwes/lite-vimeo-embed/blob/main/lite-vimeo-embed.js
-    // https://scottjehl.com/posts/even-responsiver-video/
-
-    // Get picture element or just img element if not surrounded by picture
-    const rsHeroImage = document.querySelector(`#rsHeroKPIPanel`)?.closest("picture") || document.querySelector(`#rsHeroKPIPanel`)
-    const button = document.createElement("button")
-
-    button.classList.add("rs-image-hero")
-    button.classList.add("rs-button-unset")
-    // Match the max width of the image
-    button.style.maxWidth = rsHeroImage.querySelector(`img`)?.style.maxWidth || rsHeroImage.style.maxWidth || "100%";
-
-    // Pop the image inside the button
-    rsHeroImage.parentElement.append(button)
-    button.append(rsHeroImage)
-
-    // Create the vimeo web compontent to load video and play
-    // Need to wait for component to be ready before clicking?
-    // Seems OK even on slow connection.
-    button.addEventListener("click", function (event) {
-        const vimeo = document.createElement("lite-vimeo")
-        vimeo.setAttribute("videoid", "988437576")
-        button.replaceWith(vimeo)
-        vimeo.click()
-    })
-
-    /*
      ######  ##        #######  ##     ## ########   ######
     ##    ## ##       ##     ## ##     ## ##     ## ##    ##
     ##       ##       ##     ## ##     ## ##     ## ##
@@ -60,8 +23,6 @@
     }
 
     function cloneCloud() {
-        // console.log("cloudFrequency: ", cloudFrequency);
-        console.log("Cloud!");
         const cloudTemplate = document.querySelector(`svg[data-id="rs-template-cloud"]`);
         const logoContainer = document.querySelector(".rs-logo-container");
 
@@ -72,7 +33,9 @@
         const left = Math.floor(randomNumber(1, window.innerWidth))
         cloud.style.left = `${left}px`
         const scale = Math.floor(randomNumber(1, 4))
-        cloud.style.transform = `scale(${scale}) translateY(100%)`
+        const animationSpeed = 55 / scale
+        cloud.style.transform = `scale(${scale}) translateY(-100%)`
+        cloud.style.animationDuration = `${animationSpeed}s`
         cloud.classList.add("rs-cloud-float")
         logoContainer.prepend(cloud)
     }
